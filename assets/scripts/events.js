5ed3a6e3ce74d8b68d0dbd69335eb7d382a8362c
dannyclark77 <dannyclark77@gmail.com>
2017-08-30 18:55:50 -0400

    Create X and O functionality
    Created x and o functionality in one box

---
+++ assets/scripts/events.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const position = ['#box1', '#box2', '#box3', '#box4', '#box5', '#box6', '#box7', '#box8', '#box9']
+
+let clicks = 0
+
+const clickCounter = function () {
+  clicks += 1
+}
+
+const XorO = function () {
+  clickCounter()
+  console.log(clicks)
+  if (clicks % 2 === 0) {
+    return 'X'
+  } else {
+    return 'O'
+  }
+}
+
+const play = function () {
+  $('#box1').text(XorO)
+}
+
+const click = function () {
+  $('#box1').on('click', play)
+}
+
+module.exports = {
+  click
+}
