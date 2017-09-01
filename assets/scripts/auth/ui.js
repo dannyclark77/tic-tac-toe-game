f71ee04064efbc617e1623083bad9cd085927f56
dannyclark77 <dannyclark77@gmail.com>
2017-09-01 09:15:52 -0400

    Add success-error messages for API updates
    Added success and error messages for API updates

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,37 +5,45 @@ const store = require('../store')
 const signUpSuccess = function (data) {
   console.log(data)
   console.log('Successfully signed up!')
+  $('#authMessage').text('Sign Up Successful!')
 }
 
 const signUpFailure = function (error) {
   console.error(error)
+  $('#authMessage').text('Sign Up Unsuccessful')
 }
 
 const signInSuccess = function (data) {
   console.log(data)
   console.log('Successfully signed in')
   store.user = data.user
+  $('#authMessage').text('Sign In Successful!')
 }
 
 const signInFailure = function (error) {
   console.error(error)
+  $('#authMessage').text('Sign In Unsuccessful')
 }
 
 const changePasswordSuccess = function (data) {
   console.log('Successfully changed password')
+  $('#authMessage').text('Password Successfully Changed!')
 }
 
 const changePasswordFailure = function (error) {
   console.error(error)
+  $('#authMessage').text('Password Change Unsuccessful')
 }
 
 const signOutSuccess = function (data) {
   console.log('Successfully signed out')
   store.user = null
+  $('#authMessage').text('Sign Out Successful!')
 }
 
 const signOutFailure = function (error) {
   console.error(error)
+  $('#authMessage').text('Sign Out Unsuccessful!')
 }
 
 module.exports = {
